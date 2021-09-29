3044b561ca38bc0d2b0a2f81df3a676dc9e8423e
Patrick Brennan <pbrennanmusic@gmail.com>
2021-09-28 21:20:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const form = document.getElementById('create-task-form')
-  const submitText = document.getElementById('new-task-description')
-  const tasks = document.getElementById('tasks')
+  const form = document.getElementById('create-task-form');
+  const submitText = document.getElementById('new-task-description');
+  const tasks = document.getElementById('tasks');
+
+  const dropdown = document.createElement('select');
+  dropdown.innerHTML = '<option value="high">High</option>\n<option value="medium">Medium</option>\n<option value="low">Low</option>';
+  form.appendChild(dropdown);
 
   document.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -10,23 +14,22 @@ document.addEventListener("DOMContentLoaded", () => {
     //add new list item upon submit
     const li = document.createElement('li');
     li.innerHTML = submitText.value;
+
+    //change li text color based on priority dropdown value
+    if (dropdown.value == "high") {
+      changeTextColor(li, '#FA0506')
+    } else if (dropdown.value == "medium") {
+      changeTextColor(li, '#FAF801')
+    } else if (dropdown.value == "low") {
+      changeTextColor(li, '#55FD0D')
+    }
     tasks.appendChild(li);
     form.reset();
 
-    //edit action
-    // const edit = document.createElement('button');
-    // edit.innerHTML = "Edit This Entry";
-    // li.appendChild(edit);
-    // edit.addEventListener('click', function(event) {
-    //   const input = document.createElement('input');
-    //   const submit = document.createElement('button')
-    //   submit.innerHTML = "Submit Edit"
-    //   li.appendChild(input);
-    //   li.appendChild(submit);
-    //   button.addEventListener('click', function(event) {
-    //     li.innerHTML = input.value;
-    //   });
-    // });
+
+    function changeTextColor(item, color) {
+      item.style.color = color
+    };
 
     //delete action
     const del = document.createElement('button');
@@ -36,6 +39,22 @@ document.addEventListener("DOMContentLoaded", () => {
       li.remove();
     });
 
+    //edit action
+    const edit = document.createElement('button');
+    edit.innerHTML = "Edit This Entry";
+    li.appendChild(edit);
+    edit.addEventListener('click', function(event) {
+      const input = document.createElement('input');
+      const submit = document.createElement('button')
+      submit.innerHTML = "Submit Edit"
+      li.appendChild(input);
+      li.appendChild(submit);
+      submit.addEventListener('click', function(event) {
+        li.innerHTML = input.value;
+        li.appendChild(del);
+        li.appendChild(edit);
+      });
+    });
 
   });
 });
